0cc0e579ead7559e34600e3313870949d43accfd
Pavithra <p.govinda-raj2@newcastle.ac.uk>
2025-12-31 02:19:42 +0000

    Initial commit

---
+++ reports/Coursework.r
@@ -0,0 +1,8 @@
+library(dplyr)
+getwd()
+setwd('//Users//pavithra_govinda_raj//Stats_Report')
+library(ProjectTemplate)
+create.project('MAS8600_Assignment')
+
+renv()#requirement in the report
+git()#version history is important so set up before beginning any work
